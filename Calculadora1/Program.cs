fcedbe25a7db1bd5f403b4aa69a7735a7c157ab9
rena-cruz <rena-cruz@hotmail.com>
2021-03-21 17:15:24 -0300

    commit inicial

---
+++ Calculadora1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Calculadora1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numero, total;
+            string op;
+            numero = 0;
+            total = 0;
+            op = "+";
+
+
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("------------CALCULADORA C#-------------");
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("Use simbolos matematicos para as contas");
+
+            while (op != "=") 
+            {
+                Console.WriteLine("Digite um numero qualquer: ");
+                numero = Convert.ToDouble(Console.ReadLine());
+
+                switch (op) 
+                {
+                    case "+":
+                        total = total + numero;
+                        break;
+                    case "-":
+                        total = total - numero;
+                        break;
+                    case "*":
+                        total = total * numero;
+                        break;
+                    case "/":
+                        total = total / numero;
+                        break;
+                }
+
+                Console.Write("Digite a Operaçâo Matematica: ");
+                op = Console.ReadLine();
+
+            }
+
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("O Resultado das operaçôes foi: " + total);
+            Console.WriteLine("---------------------------------------");
+            Console.ReadKey();
+        }
+    }
+}
